748b6a9686871420b80a6ffe09f037b618e89f67
GeGe-K <gloriagivondo@gmail.com>
2018-12-14 15:57:45 +0300

    folder structure, updated the project urls.

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringaschool/Documents/moringa-school-projects/core/Django/Instagram/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
