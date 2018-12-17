ad1bc8da914df8fcb7f94c22746b7051a5ab855a
GeGe-K <gloriagivondo@gmail.com>
2018-12-17 16:53:03 +0300

    added a class image

--- insta/models.py
+++ insta/models.py
@@ -13,20 +13,20 @@ class Profile(models.Model):
     Class contains user details.
     '''
 
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  bio = HTMLField()
-  website = models.CharField(max_length=30, blank=True)
-  phone_number = models.IntegerField(blank=True, null=True)
-  birth_date = models.DateField(null=True, blank=True)
-  followers = models.ManyToManyField(
-      'Profile', related_name='followers_profile', blank=True)
-  following = models.ManyToManyField(
-      'Profile', related_name='following_profile', blank=True)
-  profile_pic = models.ImageField(
-      upload_to='profile_pic/', null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = HTMLField()
+    website = models.CharField(max_length=30, blank=True)
+    phone_number = models.IntegerField(blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
+    followers = models.ManyToManyField(
+        'Profile', related_name='followers_profile', blank=True)
+    following = models.ManyToManyField(
+        'Profile', related_name='following_profile', blank=True)
+    profile_pic = models.ImageField(
+        upload_to='profile_pic/', null=True, blank=True)
 
     @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created **kwargs):
+    def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
     
@@ -47,10 +47,20 @@ class Profile(models.Model):
         profile = cls.objects.filter(user__username__icontains=name)
         return profile
 
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(id = id)
+        return profile
 
 
-# class Image(models.Model):
-#     image_path = models.ImageField(upload_to='photos/')
-#     name = models.CharField(max_length=40)
-#     caption = models.TextField()
+class Image(models.Model):
 
+    '''
+    Class contains image details
+    '''
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/', blank=True)
+    caption = HTMLField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null='True')
