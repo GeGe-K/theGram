19507f3a99d22c04b81f834b510cf653829ff916
GeGe-K <gloriagivondo@gmail.com>
2018-12-17 15:07:43 +0300

    imported imagekit and  tinymce

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,16 @@
+from imagekit.models import ProcessedImageField
 from django.db import models
+import datetime as dt
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+# class Image(models.Model):
+#     image_path = models.ImageField(upload_to='photos/')
+#     name = models.CharField(max_length=40)
+#     caption = models.TextField()
+
+
