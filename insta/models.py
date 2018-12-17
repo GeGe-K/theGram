10f2f3071097c7e223dc7e8ed5213c557fa717d5
GeGe-K <gloriagivondo@gmail.com>
2018-12-17 15:21:43 +0300

    added a class profile

--- insta/models.py
+++ insta/models.py
@@ -7,10 +7,22 @@ from django.db.models.signals import post_save
 from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  bio = HTMLField()
+  website = models.CharField(max_length=30, blank=True)
+  phone_number = models.IntegerField(blank=True, null=True)
+  birth_date = models.DateField(null=True, blank=True)
+  followers = models.ManyToManyField(
+      'Profile', related_name='followers_profile', blank=True)
+  following = models.ManyToManyField(
+      'Profile', related_name='following_profile', blank=True)
+  profile_pic = models.ImageField(
+      upload_to='profile_pic/', null=True, blank=True)
+
 
 # class Image(models.Model):
 #     image_path = models.ImageField(upload_to='photos/')
 #     name = models.CharField(max_length=40)
 #     caption = models.TextField()
-
 
